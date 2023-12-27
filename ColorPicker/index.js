545195609d13400dceb5c0a6b739260724d8c885
bukka5sandhya <bukkapatnamsandhya920@gmail.com>
2023-12-27 17:18:04 +0530

    solution

---
+++ ColorPicker/index.js
@@ -0,0 +1,20 @@
+function clickButton1(){
+    document.getElementById("colorPickerContainer").style.background ="#e0e0e0";
+    document.getElementById("selectedColorHexCode").textContent = "#e0e0e0";
+    document.getElementById("selectedColorHexCode").style.background="#49a6e9"
+}
+function clickButton2(){
+    document.getElementById("colorPickerContainer").style.background ="#6f6f6f";
+    document.getElementById("selectedColorHexCode").textContent = "#6f6f6f";
+    document.getElementById("selectedColorHexCode").style.background="#49a6e9"
+}
+function clickButton3(){
+    document.getElementById("colorPickerContainer").style.background ="#56ccf2";
+    document.getElementById("selectedColorHexCode").textContent = "#56ccf2";
+    document.getElementById("selectedColorHexCode").style.background="#49a6e9"
+}
+function clickButton4(){
+    document.getElementById("colorPickerContainer").style.background ="#bb6bd9";
+    document.getElementById("selectedColorHexCode").textContent = "#bb6bd9";
+    document.getElementById("selectedColorHexCode").style.background="#49a6e9"
+}
